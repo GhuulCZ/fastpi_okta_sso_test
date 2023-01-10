47c70b178018f6ec0a7245181047a7a0d1a711ca
Pavel Duda <pavel.duda@kyndryl.com>
2023-01-10 11:10:54 +0100

    adding server function

--- main.py
+++ main.py
@@ -8,21 +8,40 @@ import webbrowser
 SERVERPROC = None
 SSO_USERMAIL = None
 
-if __name__ == "__main__":
+
+def start_sso_login():
+    tries = 0
+    max_tries = 5
+    max_timeout = 600
+    wait_timeout = 1
+    timeout = 0
+    print_timeout = 5
     app = HTTPLocalServer()
     # queue = Queue()
     SERVERPROC = app.start_server()
     webbrowser.open("http://127.0.0.1:8000/login")
     while SERVERPROC.is_alive():
-        time.sleep(2)
         try:
             message = GLOBALQ.get(timeout=1)
             print(message)
             if message[0] == "SSO_DONE":
                 SSO_USERMAIL = message[1]
                 SERVERPROC.terminate()
+            elif message[0] == "SSO_LOGIN":
+                tries += 1
         except:
-            print("uvicorn is running")
+            if (timeout%print_timeout) == 0:
+                print(f"local web server is running (up for {timeout} seconds)")
+
+        if tries > max_tries:
+            print("maximum number of tries reached, bailing out...")
+            SERVERPROC.terminate()
+
+        time.sleep(wait_timeout)
+        timeout += wait_timeout
         
     print(f"we have usermail: {SSO_USERMAIL}")
+    return SSO_USERMAIL
 
+if __name__ == "__main__":
+    start_sso_login()
